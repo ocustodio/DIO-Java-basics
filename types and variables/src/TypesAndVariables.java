5f51797a84205cb03f19e794866dd4829795a5d3
ocustodio <cstdpvictor@gmail.com>
2024-03-06 13:18:19 -0300

    done

--- types and variables/src/TypesAndVariables.java
+++ types and variables/src/TypesAndVariables.java
@@ -1,5 +1,15 @@
 public class TypesAndVariables {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String myName = "Victor";
+        int brMinimumWage = 1412;
+        short shortNumber = 1;
+        final double PI = 3.141592; // Constant
+        boolean isTrue = false;
+
+        shortNumber = (short) brMinimumWage; // Casting
+
+        int anotherNumber = 1;
+        anotherNumber = 2;
+        System.out.println(anotherNumber);
     }
 }

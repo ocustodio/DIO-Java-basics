e4b7393dbb5732933adccca8e361c6491c389183
ocustodio <cstdpvictor@gmail.com>
2024-03-15 19:12:55 -0300

    encapsulation

---
+++ OOP/src/Messengers/Computador.java
@@ -0,0 +1,10 @@
+package Messengers;
+
+public class Computador {
+    public static void main(String[] args) {
+        MSNMessenger msn = new MSNMessenger();
+
+        msn.enviarMensagens();
+        msn.receberMensagem();
+    }
+}

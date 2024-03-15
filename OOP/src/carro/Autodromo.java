1a18f7c5b4491c86a6f3d02b12074c8731b611e6
ocustodio <cstdpvictor@gmail.com>
2024-03-15 18:03:34 -0300

    encapsulation examples

---
+++ OOP/src/carro/Autodromo.java
@@ -0,0 +1,10 @@
+package carro;
+
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+        jeep.confereCambio();
+        jeep.confereCombustivel();
+        jeep.ligar();
+    }
+}
---
+++ OOP/src/carro/Carro.java
@@ -0,0 +1,15 @@
+package carro;
+
+public class Carro {
+    public void ligar() {
+        System.out.println("Ligando carro!");
+    }
+
+    public void confereCombustivel() {
+        System.out.println("Conferindo combustivel");
+    }
+
+    public void confereCambio() {
+        System.out.println("Conferindo posicao do cambio");
+    }
+}

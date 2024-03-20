f328eda3cbdc0d37bffff971f1a7dba6ac6e6461
ocustodio <cstdpvictor@gmail.com>
2024-03-20 18:33:17 -0300

    first lesson about data sctructures

---
+++ estrutura de dados/src/com/projecto/atribuicaoreferencia/Main.java
@@ -0,0 +1,20 @@
+package com.projecto.atribuicaoreferencia;
+
+public class Main {
+    public static void main(String[] args) {
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println(intA + " " + intB);
+        intA = 2;
+        System.out.println(intA + " " + intB);
+
+        MeuObj a = new MeuObj(1);
+        MeuObj b = a;
+
+        System.out.println(a + " " + b);
+        a.setNum(2);
+        System.out.println(a + " " + b);
+
+    }
+}
---
+++ estrutura de dados/src/com/projecto/atribuicaoreferencia/MeuObj.java
@@ -0,0 +1,17 @@
+package com.projecto.atribuicaoreferencia;
+
+public class MeuObj {
+    Integer num;
+
+    public MeuObj(Integer num) {
+        this.num = num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String toString() {
+        return this.num.toString();
+    }
+}

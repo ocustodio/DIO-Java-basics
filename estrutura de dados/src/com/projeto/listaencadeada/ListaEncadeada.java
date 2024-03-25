fb8af1ff2ee41c4253ab72d29ae13da63b7af087
ocustodio <cstdpvictor@gmail.com>
2024-03-25 14:06:40 -0300

    implementing size and isEmpty methods in the linked queue

--- estrutura de dados/src/com/projeto/listaencadeada/ListaEncadeada.java
+++ estrutura de dados/src/com/projeto/listaencadeada/ListaEncadeada.java
@@ -1,4 +1,35 @@
 package com.projeto.listaencadeada;
 
-public class ListaEncadeada {
+public class ListaEncadeada<T> {
+    private No<T> referenciaEntrada;
+
+    public ListaEncadeada() {
+        this.referenciaEntrada = null;
+    }
+
+    public int size() {
+        int tamanhoLista = 0;
+        No<T> referenciaAux = referenciaEntrada;
+
+        while(true) {
+            if(referenciaAux != null) {
+                tamanhoLista++;
+                if (referenciaAux.getProximoNo() != null) {
+                    referenciaAux = referenciaAux.getProximoNo();
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        return tamanhoLista;
+    }
+
+    public boolean isEmpty() {
+        return this.referenciaEntrada == null;
+    }
+
+
 }

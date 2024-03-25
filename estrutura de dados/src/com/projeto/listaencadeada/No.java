20a61b453ec696f41fb129e2cd4af77018834f00
ocustodio <cstdpvictor@gmail.com>
2024-03-25 13:59:41 -0300

    creating knot class

--- estrutura de dados/src/com/projeto/listaencadeada/No.java
+++ estrutura de dados/src/com/projeto/listaencadeada/No.java
@@ -1,4 +1,57 @@
 package com.projeto.listaencadeada;
 
-public class No {
+public class No<T> {
+    private T conteudo;
+    private No<T> proximoNo;
+
+    public No () {
+        this.proximoNo = null;
+    }
+
+    public No (T conteudo) {
+        this.conteudo = conteudo;
+        this.proximoNo = null;
+    }
+
+    public No(T conteudo, No<T> proximoNo) {
+        this.conteudo = conteudo;
+        this.proximoNo = proximoNo;
+    }
+
+    public No<T> getProximoNo() {
+        return proximoNo;
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setProximoNo(No<T> proximoNo) {
+        this.proximoNo = proximoNo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "conteudo=" + conteudo +
+                '}';
+    }
+
+    public String toStringEncadeado() {
+        String str = "No{" +
+                "conteudo=" + conteudo +
+                '}';
+
+        if (proximoNo != null) {
+            str += " -> " + proximoNo.toString();
+        } else {
+            str += " -> null";
+        }
+
+        return str;
+    }
 }

71739e4575bf5667a42562014689229bca27418e
ocustodio <cstdpvictor@gmail.com>
2024-03-25 13:46:23 -0300

    implementing linked queue

---
+++ estrutura de dados/src/com/projeto/listaencadeada/ListaEncadeada.java
@@ -0,0 +1,4 @@
+package com.projeto.listaencadeada;
+
+public class ListaEncadeada {
+}
---
+++ estrutura de dados/src/com/projeto/listaencadeada/Main.java
@@ -0,0 +1,7 @@
+package com.projeto.listaencadeada;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

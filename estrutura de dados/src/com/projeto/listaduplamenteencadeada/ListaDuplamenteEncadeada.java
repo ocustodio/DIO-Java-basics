9bf8458586265542c0b8aff331d6f0bc9dec3761
ocustodio <cstdpvictor@gmail.com>
2024-03-26 17:11:31 -0300

    implementing getNo and get methods

--- estrutura de dados/src/com/projeto/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
+++ estrutura de dados/src/com/projeto/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -11,6 +11,20 @@ public class ListaDuplamenteEncadeada<T> {
         this.tamanhoLista = 0;
     }
 
+    public T get(int index) {
+        return this.getNo(index).getConteudo();
+    }
+
+    private No<T> getNo(int index) {
+        No<T> noAuxiliar = primeiroNo;
+
+        for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+
+        return noAuxiliar;
+    }
+
     public int size() {
         return tamanhoLista;
     }

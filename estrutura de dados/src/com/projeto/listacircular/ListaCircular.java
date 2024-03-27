fcf97f2a87530a16137c1f8520ae5968c371bad3
ocustodio <cstdpvictor@gmail.com>
2024-03-27 17:04:19 -0300

    implementing isEmpty and size methods

--- estrutura de dados/src/com/projeto/listacircular/ListaCircular.java
+++ estrutura de dados/src/com/projeto/listacircular/ListaCircular.java
@@ -1,4 +1,21 @@
 package com.projeto.listacircular;
 
-public class ListaCircular {
+public class ListaCircular<T> {
+    private No<T> cabeca;
+    private No<T> calda;
+    private int tamanhoLista;
+
+    public ListaCircular() {
+        this.cabeca = null;
+        this.calda = null;
+        this.tamanhoLista = 0;
+    }
+
+    public int size() {
+        return this.tamanhoLista;
+    }
+
+    public boolean isEmpty() {
+        return this.tamanhoLista == 0;
+    }
 }

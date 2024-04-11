60ea28cef4279c26bc07710594ef565a50d1cca0
ocustodio <cstdpvictor@gmail.com>
2024-04-10 21:30:16 -0300

    more ordering list examples

--- estrutura de dados/src/com/projeto/collections/list/OrderingListExample.java
+++ estrutura de dados/src/com/projeto/collections/list/OrderingListExample.java
@@ -2,15 +2,16 @@ package com.projeto.collections.list;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class OrderingListExample {
     public static void main(String[] args) {
         List<Gato> gatos = new ArrayList<>();
 
-        gatos.add(new Gato("Jon", null, "preto"));
+        gatos.add(new Gato("Jon", 18, "preto"));
         gatos.add(new Gato("Simba", 6, "tigrado"));
-        gatos.add(new Gato("Jhon", 12, "laranja"));
+        gatos.add(new Gato("Jon", 12, "laranja"));
 
         System.out.println(gatos);
 
@@ -19,6 +20,16 @@ public class OrderingListExample {
 
 
         Collections.sort(gatos);
+        System.out.println(gatos);
+
+//        Collections.sort(gatos, new ComparatorIdade());
+        gatos.sort(new ComparatorIdade());
+        System.out.println(gatos);
+
+        gatos.sort(new ComparatorCor());
+        System.out.println(gatos);
+
+        gatos.sort(new ComparatorNomeCorIdade());
         System.out.println(gatos);
     }
 }
@@ -63,5 +74,37 @@ class Gato implements Comparable<Gato>{
     @Override
     public int compareTo(Gato gato) {
         return this.getName().compareTo(gato.getName());
+    }
+}
+
+class ComparatorIdade implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato gato1, Gato gato2) {
+        if (gato1.getAge() < gato2.getAge()) return -1;
+        if (gato1.getAge() > gato2.getAge()) return 1;
+
+        return 0;
+    }
+}
+
+class ComparatorCor implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato gato1, Gato gato2) {
+        return gato1.getColor().compareTo(gato2.getColor());
+    }
+}
+
+class ComparatorNomeCorIdade implements Comparator<Gato> {
+    @Override
+    public int compare(Gato gato1, Gato gato2) {
+        int name = gato1.getName().compareTo(gato2.getName());
+        if (name != 0) return name;
+
+        int color = gato1.getColor().compareTo(gato2.getColor());
+        if (color != 0) return color;
+
+        return gato1.getAge().compareTo(gato2.getAge());
     }
 }
